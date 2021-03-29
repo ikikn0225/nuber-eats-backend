ddc45b8625db060aaddff160fcf8db61fcf604e1
wsx2792 <wsx2792@gmail.com>
2021-03-29 23:29:08 +0900

    21032901

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -6,10 +6,12 @@ import { JwtService } from "./jwt.service";
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 
 const TEST_KEY = 'testKey';
+const USER_ID = 1;
 
 jest.mock('jsonwebtoken', () => {
     return {
         sign: jest.fn(() => 'TOKEN'),
+        verify: jest.fn(() => ({id:USER_ID})),
     };
 });
 
@@ -34,16 +36,19 @@ describe('JwtService', () => {
     
     describe('sign', () => {
         it('should return a signed token', async() => {
-            const ID = 1;
-            const token = service.sign(1);
+            const token = service.sign(USER_ID);
             expect(typeof token).toBe("string");
             expect(jwt.sign).toHaveBeenCalledTimes(1);
-            expect(jwt.sign).toHaveBeenCalledWith({id: ID}, TEST_KEY);
+            expect(jwt.sign).toHaveBeenCalledWith({id: USER_ID}, TEST_KEY);
         });
     });
     describe('verify', () => {
         it('should return the decoded token', async() => {
-
+            const TOKEN = 'TOKEN';
+            const decodedToken = service.verify(TOKEN);
+            expect(decodedToken).toEqual({id:USER_ID});
+            expect(jwt.verify).toHaveBeenCalledTimes(1);
+            expect(jwt.verify).toHaveBeenCalledWith(TOKEN, TEST_KEY);
         });
     });
 });
