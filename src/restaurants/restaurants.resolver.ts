40af42541e503411ace7cc7d592aa4b636ce5ea9
wsx2792 <wsx2792@gmail.com>
2021-04-14 00:03:55 +0900

    21041301

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,6 +1,8 @@
+import { SetMetadata } from "@nestjs/common";
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { AuthUser } from "src/auth/auth-user.decorator";
-import { User } from "src/users/entities/user.entity";
+import { Roles } from "src/auth/role.decorator";
+import { User, UserRole } from "src/users/entities/user.entity";
 import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 import { RestaurantService } from "./restaurants.service";
@@ -11,6 +13,7 @@ export class RestaurantResolver{
     constructor(private readonly restaurantService: RestaurantService) {}
 
     @Mutation(returns => CreateRestaurantOutput)
+    @Roles(["Owner"])
     async createRestaurant( 
         @AuthUser() authUser:User,
         @Args('input') createRestaurantInput:CreateRestaurantInput,
