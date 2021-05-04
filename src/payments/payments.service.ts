b4277891f8abd3082a7e866fa22a511f3b305245
wsx2792 <wsx2792@gmail.com>
2021-05-05 08:47:38 +0900

    createPayment part Two

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,6 +1,9 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Restaurant } from "src/restaurants/entities/restaurant.entity";
+import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
+import { CreatePaymentInput, CreatePaymentOutput } from "./dtos/create-payment.dto";
 import { Payment } from "./entities/payment.entity";
 
 
@@ -9,5 +12,40 @@ export class PaymentService {
     constructor(
         @InjectRepository(Payment)
         private readonly payments: Repository<Payment>,
+        @InjectRepository(Restaurant)
+        private readonly restaurants: Repository<Restaurant>,
     ) {}
+
+    async createPayment(owner:User, {transactionId, restaurantId}:CreatePaymentInput): Promise<CreatePaymentOutput> {
+        const restaurant = await this.restaurants.findOne(restaurantId);
+        try {
+            if(!restaurant) {
+                return {
+                    ok:false,
+                    error:'Restaurant not found.',
+                };
+            }
+            if(restaurant.ownerId !== owner.id) {
+                return {
+                    ok:false,
+                    error: 'You are not allowed to do this.',
+                }
+            }
+            await this.payments.save(
+                this.payments.create({
+                transactionId,
+                user:owner,
+                restaurant,
+                }),
+            );
+            return {
+                ok:true,
+            };
+        } catch {
+            return {
+                ok:false,
+                error:"Could not create payment.",
+            }
+        }
+    }
 }
