a897a1b35d703bb418676840417635c6ef334447
wsx2792 <wsx2792@gmail.com>
2021-05-05 08:58:41 +0900

    #13.4 getPayments Resolver

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -4,6 +4,7 @@ import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
 import { CreatePaymentInput, CreatePaymentOutput } from "./dtos/create-payment.dto";
+import { GetPaymentsOutput } from "./dtos/get-payments.dto";
 import { Payment } from "./entities/payment.entity";
 
 
@@ -45,6 +46,21 @@ export class PaymentService {
             return {
                 ok:false,
                 error:"Could not create payment.",
+            }
+        }
+    }
+
+    async getPayments(user:User):Promise<GetPaymentsOutput> {
+        try {
+            const payments = await this.payments.find({ user: user });
+            return {
+                ok:true,
+                payments,
+            }
+        } catch {
+            return {
+                ok:false,
+                error:"Could not load payments.",
             }
         }
     }
