70b4c1fdc169beaf15d8057813bdd09ac690b622
wsx2792 <wsx2792@gmail.com>
2021-05-05 08:27:55 +0900

    #13.0~13.2 createPayment part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,8 @@ import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { CommonModule } from './common/common.module';
+import { PaymentsModule } from './payments/payments.module';
+import { Payment } from './payments/entities/payment.entity';
 
 
 @Module({
@@ -50,7 +52,7 @@ import { CommonModule } from './common/common.module';
       database: process.env.DB_NAME, 
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
-      entities: [User, Verification, Restaurant, Category, Dish, Order, OrderItem],
+      entities: [User, Verification, Restaurant, Category, Dish, Order, OrderItem, Payment],
     }),
     GraphQLModule.forRoot({
       installSubscriptionHandlers:true,
@@ -75,6 +77,7 @@ import { CommonModule } from './common/common.module';
     RestaurantsModule, 
     OrdersModule,
     CommonModule,
+    PaymentsModule,
  ],
   controllers: [],
   providers: [],
