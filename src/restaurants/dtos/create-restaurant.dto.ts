99e13b5a503048ab55fa7d4fd6e00fd84cf72b58
HeadCastle <wsx2792@gmail.com>
2021-05-28 11:51:41 +0900

    [fix] restaurant.entity name Length 5 cancel

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,23 +1,19 @@
-import { ArgsType, Field, InputType, Int, ObjectType, OmitType, PickType } from "@nestjs/graphql";
-import { IsBoolean, IsString, Length } from "class-validator";
-import { extend } from "joi";
-import { CoreOutput } from "src/common/dtos/output.dto";
-import { CoreEntity } from "src/common/entities/core.entity";
-import { Restaurant } from "../entities/restaurant.entity";
+import { Field, InputType, Int, ObjectType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Restaurant } from '../entities/restaurant.entity';
 
 @InputType()
 export class CreateRestaurantInput extends PickType(Restaurant, [
-    'name',
-    'coverImg',
-    'address',
+  'name',
+  'coverImg',
+  'address',
 ]) {
-    @Field(type => String)
-    categoryName: string;
+  @Field(type => String)
+  categoryName: string;
 }
-
 
 @ObjectType()
 export class CreateRestaurantOutput extends CoreOutput {
-    @Field(type => Int)
-    restaurantId?:number;
+  @Field(type => Int)
+  restaurantId?: number;
 }
