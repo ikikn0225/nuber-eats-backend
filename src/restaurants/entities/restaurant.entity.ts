e0d98bde7b6464980e17d1bf4e2b7dff3055b4ae
HeadCastle <wsx2792@gmail.com>
2021-05-31 15:42:06 +0900

    [fix] aws key change

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,66 +1,69 @@
-import { Field, InputType, ObjectType } from "@nestjs/graphql";
-import { IsBoolean, IsOptional, IsString, Length } from "class-validator";
-import { extend } from "joi";
-import { CoreEntity } from "src/common/entities/core.entity";
-import { Order } from "src/orders/entities/order.entity";
-import { User } from "src/users/entities/user.entity";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, RelationId } from "typeorm";
-import { Category } from "./category.entity";
-import { Dish } from "./dish.entity";
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Order } from 'src/orders/entities/order.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, OneToMany, RelationId } from 'typeorm';
+import { Category } from './category.entity';
+import { Dish } from './dish.entity';
 
-@InputType("RestaurantInputType", {isAbstract: true})
+@InputType('RestaurantInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant extends CoreEntity {
+  @Field(type => String)
+  @Column()
+  @IsString()
+  name: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    @Length(5)
-    name: string;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  coverImg: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    coverImg: string;
+  @Field(type => String)
+  @Column()
+  @IsString()
+  address: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    address: string;
+  @Field(type => Category, { nullable: true })
+  @ManyToOne(
+    type => Category,
+    category => category.restaurants,
+    { nullable: true, onDelete: 'SET NULL', eager: true },
+  )
+  category: Category;
 
-    @Field(type => Category, {nullable: true})
-    @ManyToOne( type => Category, category => category.restaurants, {nullable: true, onDelete:'SET NULL', eager: true,})
-    category: Category;
+  @Field(type => User)
+  @ManyToOne(
+    type => User,
+    user => user.restaurants,
+    { onDelete: 'CASCADE' },
+  )
+  owner: User;
 
-    @Field(type => User)
-    @ManyToOne( type => User, user => user.restaurants, {onDelete: 'CASCADE'})
-    owner: User;
+  @RelationId((restaurant: Restaurant) => restaurant.owner)
+  ownerId: number;
 
-    @RelationId((restaurant: Restaurant) => restaurant.owner)
-    ownerId: number;
+  @Field(type => [Order])
+  @OneToMany(
+    type => Order,
+    order => order.restaurant,
+  )
+  orders: Order[];
 
-    @Field(type => [Order])
-    @OneToMany(
-        type => Order,
-        order => order.restaurant,
-    )
-    orders: Order[];
+  @Field(type => [Dish])
+  @OneToMany(
+    type => Dish,
+    dish => dish.restaurant,
+  )
+  menu: Dish[];
 
-    @Field(type => [Dish])
-    @OneToMany(
-        type => Dish,
-        dish => dish.restaurant,
-    )
-    menu: Dish[];
+  @Field(type => Boolean)
+  @Column({ default: false })
+  isPromoted: boolean;
 
-    @Field(type => Boolean)
-    @Column({default:false})
-    isPromoted:boolean;
-
-    @Field(type => Date, { nullable:true })
-    @Column({nullable:true})
-    promotedUntil:Date;
-
-
+  @Field(type => Date, { nullable: true })
+  @Column({ nullable: true })
+  promotedUntil: Date;
 }
