05e85d85c6c756e8b4462e1c19504afb6391fa97
HeadCastle <wsx2792@gmail.com>
2021-06-04 17:21:56 +0900

    GetOrders에 id 추가

--- src/orders/dtos/get-orders.dto.ts
+++ src/orders/dtos/get-orders.dto.ts
@@ -1,9 +1,12 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Order, OrderStatus } from '../entities/order.entity';
 
 @InputType()
 export class GetOrdersInput {
+  @Field(type => Int, { nullable: true })
+  id?: number;
+
   @Field(type => OrderStatus, { nullable: true })
   status?: OrderStatus;
 }
